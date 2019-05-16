414d93a49a84e24465a2946d10bdfd55c9ff7324
Dominik Sigmund <dominik.sigmund@br.de>
2019-05-16 16:53:35 +0200

    try catch

--- index.js
+++ index.js
@@ -61,31 +61,35 @@ app.post('/form/:slug', async function (req, res) {
   if (req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
     return res.status(400).json({ 'responseCode': 1, 'responseDesc': 'Please select captcha' })
   } else {
-    var verificationUrl = 'https://www.google.com/recaptcha/api/siteverify?secret=' + config.secretKey + '&response=' + req.body['g-recaptcha-response'] + '&remoteip=' + req.connection.remoteAddress
-    let body = await request(verificationUrl) // TODO: try catch
-    body = JSON.parse(body)
-    if (body.success !== undefined && !body.success) {
-      return res.status(400).json({ 'responseCode': 1, 'responseDesc': 'Failed captcha verification' })
-    } else {
-      let results = await connection.query('SELECT id, name FROM forms WHERE slug="' + req.params.slug + '"') // TODO: try catch
-      if (results.length !== 1) {
-        res.status(500).json(results)
+    try {
+      var verificationUrl = 'https://www.google.com/recaptcha/api/siteverify?secret=' + config.secretKey + '&response=' + req.body['g-recaptcha-response'] + '&remoteip=' + req.connection.remoteAddress
+      let body = await request(verificationUrl)
+      body = JSON.parse(body)
+      if (body.success !== undefined && !body.success) {
+        return res.status(400).json({ 'responseCode': 1, 'responseDesc': 'Failed captcha verification' })
       } else {
-        let id = results[0].id
-        let name = results[0].name
-        let json = JSON.parse(req.body.date)
-        await connection.query('INSERT INTO results (form_id, json) VALUES (' + id + ', "' + JSON.stringify(json) + '")') // TODO: try catch
-        mailOptions.text = 'eq.body.body' // TODO: plain text body
-        if (json.email) {
-          mailOptions.subject = 'Your Data for ' + name
-          mailOptions.to = json.email
+        let results = await connection.query('SELECT id, name FROM forms WHERE slug="' + req.params.slug + '"')
+        if (results.length !== 1) {
+          res.status(500).json(results)
+        } else {
+          let id = results[0].id
+          let name = results[0].name
+          let json = JSON.parse(req.body.date)
+          await connection.query('INSERT INTO results (form_id, json) VALUES (' + id + ', "' + JSON.stringify(json) + '")')
+          mailOptions.text = 'eq.body.body' // TODO: plain text body
+          if (json.email) {
+            mailOptions.subject = 'Your Data for ' + name
+            mailOptions.to = json.email
+            await transporter.sendMail(mailOptions)
+          }
+          mailOptions.subject = 'New Entry for ' + name
+          mailOptions.to = config.mailformto
           await transporter.sendMail(mailOptions)
+          res.render(req.params.slug + '/form', { title: name, data: json })
         }
-        mailOptions.subject = 'New Entry for ' + name
-        mailOptions.to = config.mailformto
-        await transporter.sendMail(mailOptions)
-        res.render(req.params.slug + '/form', { title: name, data: json })
       }
+    } catch (error) {
+      res.status(500).json(error)
     }
   }
 })
